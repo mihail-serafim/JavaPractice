a6ca78f58fefa96bc2ada4bba7c6e8e96fa85110
Mihail Serafimovski <mihail.s.mk@gmail.com>
2020-10-13 17:18:08 -0400

    initial commit

---
+++ A1/src/Assignment1.java
@@ -0,0 +1,19 @@
+public class Assignment1 {
+
+	public static int[] findPeak(int[][] map){
+		return new int[0]; //Placeholder
+	}
+	
+	public static boolean isSink(int[][] map, int[] cell) {
+		return true; //Placeholder
+	}
+	
+	public static int[] findLocalSink(int[][] map, int[] startCell) {
+		return new int[0]; //Placeholder
+	}
+	
+	public static void rotateMap(int[][] map) {
+		return; //Placeholder
+	}
+	
+}
